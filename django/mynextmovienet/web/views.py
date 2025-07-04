cd1aaecf789f339fa399680be6d7cbc4c886acfd
Chase Rydeen <chaserydeen@gmail.com>
2025-07-03 20:24:38 -0500

    Successfully implemented login, needs some styling but the bones work

--- django/mynextmovienet/web/views.py
+++ django/mynextmovienet/web/views.py
@@ -1,11 +1,25 @@
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 from .model import load_model, recommend_movies_for_new_user, movie_info_lookup, get_popular_movies, train
 
-model, movie_id_to_index, index_to_movie_id = load_model()
+model, movie_id_to_index, index_to_movie_id = load_model(True)
+
+# Registration
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
+
+# Redirect to Home Page (Logged out)
+def redirect_home(request):
+    response = redirect('/app/popular-movies/')
+    return response
 
 # Create your views here.
 def information_page(request):
@@ -19,7 +33,7 @@ def get_recommendations(request):
     preferred_movie_ids = [int(movie_id) for movie_id in preferred_movie_ids]
 
     # Get recommendations
-    recommended_movie_ids = recommend_movies_for_new_user(model, preferred_movie_ids, movie_id_to_index, index_to_movie_id)
+    recommended_movie_ids = recommend_movies_for_new_user(model, preferred_movie_ids, movie_id_to_index, index_to_movie_id, True)
 
     # Return recommendations as a JSON response
     return JsonResponse({'recommended_movie_ids': recommended_movie_ids})
@@ -48,6 +62,7 @@ def get_recommendations_page(request):
             'http://127.0.0.1:8000/app/recommendations/',
             params={'movie_ids':selected_movie_ids}
         )
+        print(response)
         recommendations = response.json().get('recommended_movie_ids', [])
 
         recommendations_with_info = []
